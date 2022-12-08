3174c096bb832055bff314aa82ede2230a20a94b
JeffreySunjay <36447886+JeffreySunjay4044@users.noreply.github.com>
2022-12-08 22:11:26 +0530

    Create secrets.py
    Adding secrets file

---
+++ secrets.py
@@ -0,0 +1,33 @@
+import base64
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+region_name= "us-east-1"
+
+
+def get_secret(secret_name: str) -> dict:
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        exceptions = ['DecryptionFailureException', 'InternalServiceErrorException', 'InvalidParameterException', 'InvalidRequestException', 'ResourceNotFoundException']
+        if e.response['Error']['Code'] in exceptions:
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+    return secret  # returns the secret as dictionary
